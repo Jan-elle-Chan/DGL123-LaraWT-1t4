79344a5270c82124e27a391d77482e454c90ab87
Jan-elle-Chan <j_michelene@yahoo.com>
2023-09-11 22:47:30 -0700

    Create php file

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Demo</title>
+</head>
+<body>
+    <H1>
+    <?php 
+    echo"Hello World";
+    ?>
+    </H1>
+</body>
+</html>
